b6268c46a7014f85d4f5a363e594f86d935c90b1
Miguel Angel Ramos <return.infox@gmail.com>
2024-03-15 21:06:21 -0300

    composicion terminada con 2 objetos

--- ejercicio/02/main.py
+++ ejercicio/02/main.py
@@ -5,13 +5,25 @@ from computadora import Computadora
 
 
 def main():
+    # UN PC
     procesador_intel = Procesador('Intel Core i7', 3.8)
     ram = MemoriaRam(16, 'DDR4')
     disco_duro_ssd = DiscoDuro(512, 'SSD')
 
     mi_pc = Computadora('Mi PC Gaming', procesador_intel, ram, disco_duro_ssd)
 
-    print(mi_pc.especificaciones())
+
+    # UN MACBOOK
+    procesador_apple_silicon = Procesador('M1 pro', 3.2)
+    ram_unificada = MemoriaRam(16, 'unificada')
+    disco_duro_ssd_mac = DiscoDuro(512, 'SSD')
+
+    mac = Computadora('Macbook Pro', procesador_apple_silicon, ram_unificada, disco_duro_ssd_mac)
+
+
+    print(mi_pc.especificaciones()) # el pc
+    print(mac.especificaciones()) # Mac
+    
 if __name__ == "__main__":
     main()
 
