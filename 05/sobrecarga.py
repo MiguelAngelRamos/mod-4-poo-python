aa53d4366d7a03f2acea1b776938942fdd70202f
Miguel Angel Ramos <return.infox@gmail.com>
2024-03-20 19:41:07 -0300

    sobrecarga de metodos

--- 05/sobrecarga.py
+++ 05/sobrecarga.py
@@ -1,10 +1,11 @@
 # class Calculadora:
-#     def sumar(self, numero_1, numero_2, numero_3 = 0):
-#         return numero_1 + numero_2 + numero_3
+#     def sumar(self, numero_1, numero_2, numero_3 = 0, numero_4 = 0):
+#         return numero_1 + numero_2 + numero_3 + numero_4
 
 # calculadora = Calculadora()
 # print(calculadora.sumar(2,3))
 # print(calculadora.sumar(2,3,6))
+# print(calculadora.sumar(10, 20, 40, 100))
 
 class Calculadora:
     def sumar(self, *args):
